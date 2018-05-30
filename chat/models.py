55fb149ee49fde2e27a341efd6e6b5f518c8934a
Caitlin <caitlinchou@gmail.com>
2018-05-30 17:38:02 -0400

    fix profile picture

--- chat/models.py
+++ chat/models.py
@@ -1,7 +1,3 @@
-from PIL import Image
-import requests
-from io import BytesIO
-
 from django.db import models
 
 # Create your models here.
@@ -19,7 +15,7 @@ class User(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     status = models.TextField()
-    #profile_picture = models.ImageField(default=Image.open(BytesIO(requests.get('https://www.ienglishstatus.com/whatsapp-dp-images-profile-pictures/').content)))
+    profile_picture = models.ImageField(null=True, blank=True)
     group = models.ManyToManyField(Group)
 
     def __str__(self):
