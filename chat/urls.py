7a37a49a4769da375ef6292f04dfdda1c0c6df85
Caitlin <caitlinchou@gmail.com>
2018-06-11 10:15:41 -0400

    login works

--- chat/urls.py
+++ chat/urls.py
@@ -6,4 +6,6 @@ app_name = "chat"
 
 urlpatterns = [
     url(r'^$', views.main, name='main'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^login_submit/$', views.login_submit, name='login_submit'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,16 +1,26 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 
 # Create your views here.
+from django.urls import reverse
 
 
-def main(request):
+def login(request):
+    return render(request, 'chat/index.html')
+
+
+def main(request, user=None):
     context = {
-        'title': 'it worked'
+        'user': user
     }
 
-    return render(request, 'chat/index.html', context)
+    if not user:
+        return HttpResponseRedirect(reverse('blog:login'))
+
+    return render(request, 'chat/main.html', context)
 
 
-
-#
+def login_submit(request):
+    return HttpResponseRedirect(reverse('blog:main',
+                                        kwargs={'user': request.POST['username']}))
